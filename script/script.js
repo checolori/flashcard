e1d9a7268cbf23f9160223d72b85f4aeec14fc95
checolori <dario.antonioli@gmail.com>
2018-10-19 11:49:33 +0200

    structure

---
+++ script/script.js
@@ -0,0 +1,31 @@
+function changeText() {
+document.getElementById('boldStuff').innerHTML = 'The DOM is ';
+}
+
+//document.getElementById("myBtn").addEventListener("click", function changeColor() {
+//  document.body.style.color = "purple";
+//  return false;
+//});
+
+//const hello = document.getElementById("myBtn").addEventListener("click", function(){
+// alert("Hello World!");
+//});
+
+
+
+
+// create a new div element 
+// const message = document.createElement('h1')
+// // // and give it some content 
+// message.innerHTML = 'Hello World'
+// message.id = 'message'
+// // // add the text node to the newly created div
+
+// const body = document.getElementsByTagName('body')[0]
+// body.appendChild(message)
+
+
+/*
+
+
+*/
